6a872db233cfe4d26b720b1a2fe544606c8ebcf4
TridentTrue <adam.donaldson101@gmail.com>
2020-09-15 22:33:38 +0100

    Move ContextFactory into its own file

--- WeatherForecast.Infrastructure/Persistence/WeatherForecastContext.cs
+++ WeatherForecast.Infrastructure/Persistence/WeatherForecastContext.cs
@@ -14,21 +14,3 @@ namespace WeatherForecast.Infrastructure.Persistence
 		}
 	}
 }
-
-// This section is required for development of the database using EF
-namespace WeatherForecast.Infrastructure
-{
-	using Microsoft.EntityFrameworkCore.Design;
-	using WeatherForecast.Infrastructure.Persistence;
-
-	public class GroceriesContextFactory : IDesignTimeDbContextFactory<WeatherForecastContext>
-    {
-        public WeatherForecastContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<WeatherForecastContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=WeatherForecast;Trusted_Connection=True;MultipleActiveResultSets=true");
-
-            return new WeatherForecastContext(optionsBuilder.Options);
-        }
-    }
-}
---
+++ WeatherForecast.Infrastructure/Persistence/WeatherForecastContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WeatherForecast.Infrastructure.Persistence
+{
+    // This factory class is required for development of the database using EF
+    public class WeatherForecastContextFactory : IDesignTimeDbContextFactory<WeatherForecastContext>
+    {
+        public WeatherForecastContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WeatherForecastContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=WeatherForecast;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new WeatherForecastContext(optionsBuilder.Options);
+        }
+    }
+}
