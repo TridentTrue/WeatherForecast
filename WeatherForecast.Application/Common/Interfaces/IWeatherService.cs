fe2c96c117eef08e445ac3c5ad47a97c1776e7c6
TridentTrue <adam.donaldson101@gmail.com>
2020-09-15 22:34:37 +0100

    Add API response models. Add IWeatherService interface.

---
+++ WeatherForecast.Application/Common/Interfaces/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecast.Application.Common.Models;
+
+namespace WeatherForecast.Application.Common.Interfaces
+{
+	public interface IWeatherService
+	{
+		Task<FiveDayForecastModel> GetFiveDayForecast(int woeid);
+	}
+}
