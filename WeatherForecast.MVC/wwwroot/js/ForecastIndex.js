249bbac43852fe0119f641c7829d07a6fc6701bf
TridentTrue <adam.donaldson101@gmail.com>
2020-09-16 19:09:44 +0100

    Implement PullToRefresh.js

--- WeatherForecast.MVC/wwwroot/js/ForecastIndex.js
+++ WeatherForecast.MVC/wwwroot/js/ForecastIndex.js
@@ -1,6 +1,11 @@
-﻿document.getElementById("reload").addEventListener('click', ReloadForecast);
+﻿import PullToRefresh from "../lib/pulltorefreshjs/dist/index.esm.js"
 
 function ReloadForecast() {
+	// display loader
+	const loader = document.getElementById("loader");
+	loader.style.visibility = "visible";
+
+	// attempt to reload the ViewComponent
 	fetch(Url_FiveDayForecast).then(function (response) {
 		// fetch successful, return html string
 		return response.text();
@@ -8,6 +13,20 @@ function ReloadForecast() {
 		// set wrapper as html
 		document.getElementById("forecast-wrapper").innerHTML = html;
 	}).catch(function (err) {
+		// display the error if there is one
 		alert("error: reload unsuccessful", err);
+	}).finally(function () {
+		// remove the loader
+		loader.style.visibility = "hidden";
 	});
 }
+
+document.getElementById("reload").addEventListener("click", ReloadForecast);
+
+// from https://github.com/BoxFactura/pulltorefresh.js
+const ptr = PullToRefresh.init({
+	mainElement: "body",
+	onRefresh() {
+		ReloadForecast();
+	}
+});
